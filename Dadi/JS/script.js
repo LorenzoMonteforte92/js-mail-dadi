20fcc568b67d82cd5128e9fd0c17e6a94a869ce6
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-03-08 15:57:02 +0100

    fix: script sheet

--- Dadi/JS/script.js
+++ Dadi/JS/script.js
@@ -2,6 +2,17 @@
 const UserNumber = Math.floor(Math.random() * 6) + 1;
 // generare numero random per pc da 1 a 6 
 const cpuNumber = Math.floor(Math.random() * 6) + 1;
+console.log(UserNumber);
+console.log(cpuNumber);
+
 // verificare quale numero è più grande e se vince utente o pc
-console.log(UserNumber)
-console.log(cpuNumber)
+let userMessage;
+if(UserNumber > cpuNumber){
+    userMessage = `Complimenti hai vinto`
+} else if (cpuNumber < UserNumber){
+    userMessage = `Hai perso CPU vince`
+}else if(cpuNumber === UserNumber){
+    userMessage = `Pareggio`
+};
+
+console.log(userMessage);
