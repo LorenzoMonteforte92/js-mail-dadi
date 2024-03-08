7a34473a8427dfa865dcd04a6b81f20174676b4b
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-03-08 16:00:46 +0100

    fix: dice game

--- Dadi/JS/script.js
+++ Dadi/JS/script.js
@@ -2,16 +2,16 @@
 const UserNumber = Math.floor(Math.random() * 6) + 1;
 // generare numero random per pc da 1 a 6 
 const cpuNumber = Math.floor(Math.random() * 6) + 1;
-console.log(UserNumber);
-console.log(cpuNumber);
+console.log(`User tira: ` + UserNumber);
+console.log(`CPU tira: ` + cpuNumber);
 
 // verificare quale numero è più grande e se vince utente o pc
 let userMessage;
 if(UserNumber > cpuNumber){
-    userMessage = `Complimenti hai vinto`
-} else if (cpuNumber < UserNumber){
+    userMessage = `Complimenti hai vinto!!`
+} else if (UserNumber < cpuNumber){
     userMessage = `Hai perso CPU vince`
-}else if(cpuNumber === UserNumber){
+}else if(UserNumber === cpuNumber){
     userMessage = `Pareggio`
 };
 
